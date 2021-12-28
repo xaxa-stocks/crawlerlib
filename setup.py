d7335d6ab1f7be09093bddc993214b35eb321f78
Marcos Xaxa <marcos@xaxas.net>
2021-12-28 07:41:35 -0300

    adding packages

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='fiicrawler',
-    packages=find_packages(),
+    # packages=find_packages(),
     version='0.0.1',
     description='Lib to scrap the web for fiis` information',
     author='Marcos Xaxá',
